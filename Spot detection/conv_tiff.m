1b878c47edf6d643c8a51a0422214e457533bd55
Angie Perez <biotec.girl1986@gmail.com>
2016-10-28 14:06:45 +0200

    Update conv_tiff.m

--- Spot detection/conv_tiff.m
+++ Spot detection/conv_tiff.m
@@ -8,3 +8,16 @@ function newfilename=conv_tiff(fileName,framenum)
     end
     fclose(fp);
     newfilename=[fileName '_interleaved'];
+    
+    
+    @@@ Changes to the code 
+    function newfilename=conv_tiff(fileName,framenum)
+    info=imfinfo([fileName '.tif']);
+    fp = fopen([fileName '.tif'], 'rb');
+    fseek(fp, info.StripOffsets, 'bof');
+    for cnt = 1:framenum
+        imData = fread(fp, [info.Width info.Height], 'uint8', 0, 'ieee-be')';
+        imwrite(imData,[fileName '_interleaved.tif'],'tif','compression','none','writemode','append');
+    end
+    fclose(fp);
+newfilename=[fileName '_interleaved'];
